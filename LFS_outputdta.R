a7254c5a2017b1bb4f1742b5f1114ff1dfa7c8c7
Tung Saisawat Samutpradit <s.sai914@gmail.com>
2020-07-28 17:00:26 +0900

    add HH_no, members

--- LFS_outputdta.R
+++ LFS_outputdta.R
@@ -1,6 +1,6 @@
 #=================================================================================
 # This program selects only necessary variables for stata and renames for merge
-# last update - 3/9/2020
+# last update - 7/28/2020
 # input       - _selected.csv annual data 2001-2018
 # output      - _ana.csv annual data 2001-2018
 # WORKFLOW    - get column names and store in a list
@@ -9,27 +9,31 @@
 #             - merge and output as a single .dta file
 #=================================================================================
 
+library(dplyr)
 library(haven)
-library(tidyverse)
 
 rm(list = ls(all.names = TRUE))
-setwd("T:/Data/LFS")
+setwd("C:\\Users\\81804\\Desktop\\Data\\LFS")
 
 # =============================================================================
 
-file <- list.files(path = "/Data/LFS/",
-                  "_selected.csv",
+file <- list.files(path = getwd(),
+                   "_selected.csv",
                   full.names = TRUE) #This is LFS by year
 
 collist <- list()
 
 for (i in 1:length(file)) {
-  temp <- read.csv(file[i], row.names = 1)
-  collist[[i]] <- colnames(temp)
-  message <- paste0("Obtained colname of data no. ", i)
+  temp = read.csv(file[i], row.names = 1)
+  collist[[i]] = colnames(temp)
+  message = paste0("Obtained colname of data no. ", i)
   print(message)
   rm(temp, message, i)
 }
+
+save(collist, file = "collist.Rdata")
+
+# drop 2001-2002 because there is no HH no
 
 # =============================================================================
 
@@ -44,6 +48,7 @@ checkvar = function(v){
 }
 
 varneed = list("year", "quarter", "CWT", "MONTH",
+               "HH_NO", "MEMBERS",
                "SEX", "AGE", "MARITAL",
                "STATUS", "INDUS", "SIZE_", "OCCUP",
                "RE_ED",
@@ -59,6 +64,15 @@ for (i in 1: length(varneed)) {
 # [1] "Change variable to CWT for year 2001"
 # [1] "Change variable to CWT for year 2002"
 # [1] "Change variable to CWT for year 2003"
+# [1] "Change variable to HH_NO for year 2001"
+# [1] "Change variable to HH_NO for year 2002"
+# [1] "Change variable to MEMBERS for year 2010"
+# [1] "Change variable to MEMBERS for year 2013"
+# [1] "Change variable to MEMBERS for year 2014"
+# [1] "Change variable to MEMBERS for year 2015"
+# [1] "Change variable to MEMBERS for year 2016"
+# [1] "Change variable to MEMBERS for year 2017"
+# [1] "Change variable to MEMBERS for year 2018"
 # [1] "Change variable to STATUS for year 2013"
 # [1] "Change variable to INDUS for year 2013"
 # [1] "Change variable to SIZE_ for year 2013"
@@ -79,13 +93,12 @@ for (i in 1: length(varneed)) {
 
 # =============================================================================
 
-for (i in 1: length(file)){
-  data = read.csv(file[i], row.names = 1)
+for (i in 3: length(file)){
+  data <- read.csv(file[i], row.names = 1)
   if(i %in% 1:2){
     newdata = rename(data,
                      CWT = CWD,
                      AMOUNT = AMOUMT)
-   
   }  else if (i == 3) {
     newdata = rename(data,
                      CWT = CWD,
@@ -97,6 +110,9 @@ for (i in 1: length(file)){
     newdata = rename(data,
                      RE_WK = RE_MK,
                      AMOUNT = AMOUMT)
+  } else if (i==10) {
+    newdata = rename(data,
+                     MEMBERS = MEMBERS_)
   } else if (i==13){
     newdata = rename(data,
                      STATUS = status,
@@ -104,15 +120,20 @@ for (i in 1: length(file)){
                      SIZE_ = size,
                      MAIN_HR = main_hr,
                      OTHER_HR = other_hr,
-                     TOTAL_HR = total_hr)
+                     TOTAL_HR = total_hr,
+                     MEMBERS = MEMBERS_)
+  } else if (i %in% 14:18){
+    newdata = rename(data,
+                     MEMBERS = MEMBERS_)
   } else {
     newdata = data
   }
   rm(data)
-  msg1 = paste0("Successfully renamed data no.", i)
+  msg1 <- paste0("Successfully renamed data no.", i)
   print(msg1)
-  output = select(newdata, 
+  output <- select(newdata, 
                   year, quarter, CWT, MONTH,
+                  HH_NO, MEMBERS,
                   SEX, AGE, MARITAL,
                   STATUS, INDUS, SIZE_, OCCUP,
                   RE_ED,
@@ -121,127 +142,24 @@ for (i in 1: length(file)){
                   MAIN_HR, OTHER_HR, TOTAL_HR,
                   BONUS, OT, OTH_MONEY
                   )
-  year = output$year[1]
-  outputname = paste0("LFS_",year, "_ana.csv")
+  year <- output$year[1]
+  outputname <- paste0("LFS_",year, "_ana.csv")
   write.csv(output, file = outputname)
-  msg2 = paste0("Successfully output data no.", i)
+  msg2 <- paste0("Successfully output data no.", i)
   print(msg2)
   rm(newdata, output, outputname, msg1, msg2, i, year)
 }
 
-rm(collist, varneed, file)
-
-# recode industry ==============================================================
-
-library(readxl)
-concordance <- read_excel("TSIC_to_ISIC_edited.xlsx") %>%
-  select(-c(oneDigit)) #1389 obs
-
-# concordance <- read_excel("LFS_concordance.xlsx") #1389 obs
-
-ISIC_match <- concordance %>%
-  select(c(ISIC, bigGroup)) %>%
-  na.omit() %>%
-  distinct() %>%
-  rename(indus.original = ISIC) %>%
-  mutate(indus.original = as.numeric(indus.original)) #296 unique correspondence
-
-LFS11_match <- concordance %>%
-  select(c(LFS11, bigGroup))  %>%
-  na.omit() %>%
-  distinct() %>%
-  rename(indus.original = LFS11) %>%
-  mutate(indus.original = as.numeric(indus.original)) #441 unique correspondence
-
-TSIC_match <- concordance %>%
-  select(c(TSIC, bigGroup))  %>%
-  na.omit() %>%
-  distinct() %>%
-  rename(indus.original = TSIC) %>%
-  mutate(indus.original = as.numeric(indus.original)) #1089 unique correspondence
-
-# check if there is one to many match
-
-ISIC_match$dup <- duplicated(ISIC_match$indus.original)
-LFS11_match$dup <- duplicated(LFS11_match$indus.original)
-TSIC_match$dup <- duplicated(TSIC_match$indus.original)
-
-# errorRow <- concordance[concordance$dup == TRUE, ] #302 rows
-
-ISICerror <- ISIC_match[ISIC_match$dup == TRUE,] #0 errors
-LFS11error <- LFS11_match[LFS11_match$dup == TRUE,] #0 errors
-TSICerror <- TSIC_match[TSIC_match$dup == TRUE,] #0 errors
-
-matchList <- list(ISIC_match, LFS11_match, TSIC_match)
-
-# recode 2001-2010, 2012-2018
-
-for (year in 2001:2018) {
-  input <- paste0("LFS_", year, "_ana.csv")
-  data <- read.csv(input, header = TRUE)
-  
-  m <- ifelse(year <= 2010, 1,
-              ifelse(year == 2011, 2, 3))
-  
-  data_indusRecode <- merge(data, matchList[[m]],
-                            by.x = "INDUS", by.y = "indus.original",
-                            all.x = TRUE, all.y = FALSE)
-  if (nrow(data) == nrow(data_indusRecode)) {
-    outputname <- paste0("LFS_",year, "_indusRecode.csv")
-    write.csv(data_indusRecode, file = outputname)
-    msg <- paste0("output year ", year)
-    print(msg)
-    } else {
-    msg <- paste0("error for year ", year)
-    print(msg)
-  }
-  
-  if (year == 2018) {
-    rm(input, data, data_indusRecode, outputname, msg)
-  }
-}
-
-# sample ========
-sampleData <- read.csv("LFS_2001_indusRecode.csv", row.names = 1)
-errorData <- sampleData[which(is.na(sampleData$bigGroup) == TRUE & 
-                             is.na(sampleData$INDUS) == FALSE), ]
-
-table(errorData$INDUS)
-
 # ==============================================================================
 
-file_recode = list.files("/Data/LFS",
-                      pattern = "_indusRecode.csv",
+file_ana <- list.files(pattern = "_ana.csv",
                       full.names = TRUE)
 
 # This line may take some time to run
-LFS_all <- lapply(file_recode, read.csv, row.names=1) %>%
-  bind_rows() %>%
-  rename(industry = bigGroup) %>%
-  select(-c(dup))
+allLFS_ana <- lapply(file_ana[3:18], read.csv, row.names=1)
+LFS_all_ana <- bind_rows(allLFS_ana)
 
-# Find pattern in industry data that could not be matched
-errorData <- LFS_all[which(is.na(LFS_all$industry) == TRUE & 
-                                 is.na(LFS_all$INDUS) == FALSE), ]
+write_dta(LFS_all_ana, 
+          path = "./LFS_all.dta")
 
-
-table(errorData$INDUS)
-
-# Find if there is any jump in industry at 2010-2011, 2011-2012
-
-jumpCheck <- function(x){
-  jumpData <- LFS_all[LFS_all$industry == x,3] %>%
-    table() %>%
-    as.data.frame() %>%
-    rename(year = ".")
-  
-  p <- plot(jumpData$year, jumpData$Freq)
-  return(p)
-}
-
-oneDigit <- levels(LFS_all$industry)
-sapply(oneDigit, jumpCheck)
-
-# output data
-write_dta(LFS_all, 
-          path = "/Data/LFS/LFS_all.dta")
+rm(allLFS_ana, file_ana)
